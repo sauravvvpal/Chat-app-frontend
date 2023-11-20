2ae27eec8abc6877f27cbe187439bf315e95910f
Saurav Pal <sauravpal2001@gmail.com>
2023-11-20 11:17:28 +0530

    name changed

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -141,7 +141,7 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Chat Buddy
         </Text>
         <div>
           <Menu>
